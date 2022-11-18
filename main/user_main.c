448710c8c362ca980cf3ff244224657390c0a94f
FahadHosein <fahad.hosein@my.uwi.edu>
2022-11-17 21:24:48 -0400

    Updated user_main, created .gitignore.

--- main/user_main.c
+++ main/user_main.c
@@ -2,7 +2,7 @@
 
 void app_main(void)
 {
-//    unit_test_gpio_on(NULL);
+//   unit_test_gpio_on(NULL);
 //   integration_test_gpio_on(NULL);
-    verification_test_gpio_on(NULL);
+   verification_test_gpio_on(NULL);         // Remove the "break;" at the end of "gpio_on" function in "user_main.h" for this to work
 }
